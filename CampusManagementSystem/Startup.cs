f995859e93b5b5f8852135345830463dee35846a
mTAN WEE QUAN <weequan@live.com>
2016-03-20 00:13:12 +0800

    initial file

---
+++ CampusManagementSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CampusManagementSystem.Startup))]
+namespace CampusManagementSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
