f7b5f64cace47ac28b3794a38e6a66ab814c4cd2
mTAN WEE QUAN <weequan@live.com>
2016-03-28 00:13:01 +0800

    done login and redirect view

--- CampusManagementSystem/Controllers/HomeController.cs
+++ CampusManagementSystem/Controllers/HomeController.cs
@@ -1,30 +1,164 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using CampusManagementSystem.Models;
 
 namespace CampusManagementSystem.Controllers
 {
     public class HomeController : Controller
     {
+        private CMSDBContext db = new CMSDBContext();
+
+        // GET: Home
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "Id,Acid_Password")] User user)
         {
-            ViewBag.Message = "Your application description page.";
+            if (ModelState.IsValid)
+            {
+                User userr = db.Users.Find(user.Id);
+                if (userr == null)
+                {
+                    return HttpNotFound();
+                }
+                else
+                {
+                    if (userr.Acid_Password.Equals(user.Acid_Password))
+                    {
+                        Session["ID"] = user.Id;
+                        if (userr.Type.Equals("0"))
+                        {
+                            return RedirectToAction("Index", "Admin");
+                        }
+                        else if (userr.Type.Equals("1"))
+                        {
+                            return RedirectToAction("Index", "Lecutrer");
+                        }
+                        else if (userr.Type.Equals("3"))
+                        {
+                            return RedirectToAction("Index", "Students");
+                        }
+                    }
+                    return HttpNotFound();
+                    
+                }
+            }
 
+            return View(user);
+        }
+
+        // GET: Home/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // GET: Home/Create
+        public ActionResult Create()
+        {
             return View();
         }
 
-        public ActionResult Contact()
+        // POST: Home/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Type,Acid_Password")] User user)
         {
-            ViewBag.Message = "Your contact page.";
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
 
-            return View();
+            return View(user);
+        }
+
+        // GET: Home/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Home/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Type,Acid_Password")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        // GET: Home/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Home/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            User user = db.Users.Find(id);
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
