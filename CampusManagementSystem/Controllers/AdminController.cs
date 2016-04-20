f2ed2761c7bf788c829b300adb16712a694a60e0
mTAN WEE QUAN <weequan@live.com>
2016-04-20 23:24:30 +0800

    Admin Done

--- CampusManagementSystem/Controllers/AdminController.cs
+++ CampusManagementSystem/Controllers/AdminController.cs
@@ -7,6 +7,8 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using CampusManagementSystem.Models;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace CampusManagementSystem.Controllers
 {
@@ -14,6 +16,9 @@ namespace CampusManagementSystem.Controllers
     {
         private NewwsContext db2 = new NewwsContext();
         private CoursesContext db = new CoursesContext();
+        private ClassesContext db3 = new ClassesContext();
+        private StudentsContext db4 = new StudentsContext();
+        private TakensContext db5 = new TakensContext();
         // GET: Admin
         public ActionResult Index()
         {
@@ -164,11 +169,21 @@ namespace CampusManagementSystem.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             Course course = db.Courses.Find(id);
+            
             if (course == null)
             {
                 return HttpNotFound();
             }
-            return View(course);
+
+            var query = "SELECT * FROM dbo.Classes where Code='" + course.Code+"'";
+
+            using (var context = new ClassesContext())
+            {
+                var blogs = context.Classes.SqlQuery(query).ToList();
+                return View(blogs);
+            }
+            
+            
         }
 
         // GET: Admins/CoursesCreate
@@ -249,6 +264,302 @@ namespace CampusManagementSystem.Controllers
             db.Courses.Remove(course);
             db.SaveChanges();
             return RedirectToAction("Courses");
+        }
+
+        public ActionResult ClassCreate()
+        {
+            return View();
+        }
+
+        // POST: Admins/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ClassCreate([Bind(Include = "Id,Code,Section,Student")] Classe classe)
+        {
+            if (ModelState.IsValid)
+            {
+                db3.Classes.Add(classe);
+                db3.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(classe);
+        }
+        // GET: Admins/Details/5
+        public ActionResult ClassDetails(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Classe classe = db3.Classes.Find(id);
+            if (classe == null)
+            {
+                return HttpNotFound();
+            }
+            var currentyear = "2016";
+            var query = "SELECT * FROM dbo.Takens where Code='" + classe.Code + "' AND Year='"+currentyear+"' AND Section ='"+classe.Section+"'";
+
+            using (var context = new TakensContext())
+            {
+                var blogs = context.Takens.SqlQuery(query).ToList();
+                return View(blogs);
+            }
+            
+        }
+
+
+        // GET: Admins/Edit/5
+        public ActionResult ClassEdit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Classe classe = db3.Classes.Find(id);
+            if (classe == null)
+            {
+                return HttpNotFound();
+            }
+            return View(classe);
+        }
+
+        // POST: Admins/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ClassEdit([Bind(Include = "Id,Code,Section,Student")] Classe classe)
+        {
+            if (ModelState.IsValid)
+            {
+                db3.Entry(classe).State = EntityState.Modified;
+                db3.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(classe);
+        }
+
+        // GET: Admins/Delete/5
+        public ActionResult ClassDelete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Classe classe = db3.Classes.Find(id);
+            if (classe == null)
+            {
+                return HttpNotFound();
+            }
+            return View(classe);
+        }
+
+        // POST: Admins/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult ClassDeleteConfirmed(int id)
+        {
+            Classe classe = db3.Classes.Find(id);
+            db3.Classes.Remove(classe);
+            db3.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Students()
+        {
+            return View(db4.Students.ToList());
+        }
+
+        // GET: Admins/Details/5
+        public ActionResult StudentsDetails(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db4.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // GET: Admins/Create
+        public ActionResult StudentsCreate()
+        {
+            return View();
+        }
+
+        // POST: Admins/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult StudentsCreate([Bind(Include = "Id,Acid,Year")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db4.Students.Add(student);
+                db4.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(student);
+        }
+
+        // GET: Admins/Edit/5
+        public ActionResult StudentsEdit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db4.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Admins/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult StudentsEdit([Bind(Include = "Id,Acid,Year")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(student).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+
+        // GET: Admins/Delete/5
+        public ActionResult StudentsDelete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db4.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Admins/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult StudentsDeleteConfirmed(int id)
+        {
+            Student student = db4.Students.Find(id);
+            db4.Students.Remove(student);
+            db4.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        // GET: Admins/Details/5
+        public ActionResult TakenDetails(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Taken taken = db5.Takens.Find(id);
+            if (taken == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taken);
+        }
+
+        // GET: Admins/Create
+        public ActionResult TakenCreate()
+        {
+            return View();
+        }
+
+        // POST: Admins/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult TakenCreate([Bind(Include = "Id,Code,Acid,Section,Result,Year")] Taken taken)
+        {
+            if (ModelState.IsValid)
+            {
+                db5.Takens.Add(taken);
+                db5.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(taken);
+        }
+
+        // GET: Admins/Edit/5
+        public ActionResult TakenEdit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Taken taken = db5.Takens.Find(id);
+            if (taken == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taken);
+        }
+
+        // POST: Admins/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult TakenEdit([Bind(Include = "Id,Code,Acid,Section,Result,Year")] Taken taken)
+        {
+            if (ModelState.IsValid)
+            {
+                db5.Entry(taken).State = EntityState.Modified;
+                db5.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(taken);
+        }
+
+        // GET: Admins/Delete/5
+        public ActionResult TakenDelete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Taken taken = db5.Takens.Find(id);
+            if (taken == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taken);
+        }
+
+        // POST: Admins/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult TakenDeleteConfirmed(int id)
+        {
+            Taken taken = db5.Takens.Find(id);
+            db5.Takens.Remove(taken);
+            db5.SaveChanges();
+            return RedirectToAction("Index");
         }
 
     }
---
+++ CampusManagementSystem/Controllers/AdminsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CampusManagementSystem.Models;
+
+namespace CampusManagementSystem.Controllers
+{
+    public class AdminsController : Controller
+    {
+        private TakensContext db = new TakensContext();
+
+        // GET: Admins
+        public ActionResult Index()
+        {
+            return View(db.Takens.ToList());
+        }
+
+        // GET: Admins/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Taken taken = db.Takens.Find(id);
+            if (taken == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taken);
+        }
+
+        // GET: Admins/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admins/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Code,Acid,Section,Result,Year")] Taken taken)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Takens.Add(taken);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(taken);
+        }
+
+        // GET: Admins/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Taken taken = db.Takens.Find(id);
+            if (taken == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taken);
+        }
+
+        // POST: Admins/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Code,Acid,Section,Result,Year")] Taken taken)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(taken).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(taken);
+        }
+
+        // GET: Admins/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Taken taken = db.Takens.Find(id);
+            if (taken == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taken);
+        }
+
+        // POST: Admins/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Taken taken = db.Takens.Find(id);
+            db.Takens.Remove(taken);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
